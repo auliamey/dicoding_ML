2f63a664ad16c59e69d8b6dc7544d051af2c4e05
auliamey <auliaannandya@gmail.com>
2024-12-07 02:32:21 +0700

    fix

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const { Storage } = require("@google-cloud/storage");
 const admin = require("firebase-admin");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 
 app.use(bodyParser.json());
 
